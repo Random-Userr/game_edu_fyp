de8233f74667429f206c30d3209009796556713a
Swaminathan <91941849+Random-Userr@users.noreply.github.com>
2024-02-09 19:59:03 +0530

    Update startup_page.dart

--- lib/memGameUi/memGamePages/startup_page.dart
+++ lib/memGameUi/memGamePages/startup_page.dart
@@ -4,11 +4,11 @@ import 'package:game_edu_fyp/MemGameUi/MemGameWidgets/game_options.dart';
 import 'package:game_edu_fyp/MemGameUtils/constants.dart';
 
 class StartUpPage extends StatelessWidget {
-  const StartUpPage({super.key});
+  const StartUpPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Center(
           child: Column(
@@ -23,6 +23,31 @@ class StartUpPage extends StatelessWidget {
               ]),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showGameDescription(context),
+        child: Icon(Icons.help),
+      ),
+    );
+  }
+
+  void _showGameDescription(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Memory Flip Card Game"),
+          content: Text(
+              "The Memory Flip Card Game is a classic concentration game where players need to match pairs of cards by flipping them over. It's a great way to improve memory, focus, and cognitive skills."),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text("Close"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
